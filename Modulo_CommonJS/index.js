6c97bad98d7e39919e386f7bcda566d90c673517
Pedro Corrêa <142067181+pedro-correax@users.noreply.github.com>
2024-08-22 21:40:03 -0400

    Delete Modulo_CommonJS directory

--- Modulo_CommonJS/index.js
+++
@@ -1,5 +0,0 @@
-const matematica = require('./math.js');
-
-
-console.log(matematica.soma(1, 2));
-console.log(matematica.subtracao(1, 2));
--- Modulo_CommonJS/math.js
+++
@@ -1,9 +0,0 @@
-function soma(a, b) {
-    return a + b;
-}
-
-const subtracao = (a, b) => {
-    return a - b;
-}
-
-module.exports = { soma, subtracao }
